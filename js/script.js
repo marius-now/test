04d63835e5fd75d9e72e71a38d243e34054d547f
Marius <example@gmail.com>
2023-09-04 23:59:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const storeName = "linella";
+
+const storeDescription = {
+  buget: 10000,
+  employees: ["ion", "marius", "valentin"],
+  products: { banana: 200, mivina: 2 },
+  open: true,
+};
+
+alert("alert");
+const result = confirm("totul e bine?");
+console.log(result);
